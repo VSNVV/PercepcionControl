5b60aa6352a607724cf1821876fc025071e2cddd
viti <victor.sanavia@edu.uah.es>
2024-02-12 21:44:22 +0100

    Correcciones en SensorLineal.m
    Ahora no medimos la distancia recorrida, medimos el numero de muestras que tenemos para calcular la resolución, y mostramos la máxima resolución, la mínima, y la media de todas ellas

--- Práctica 1/SensorLineal.m
+++ Práctica 1/SensorLineal.m
@@ -11,7 +11,7 @@ pub = rospublisher('/robot0/cmd_vel', 'geometry_msgs/Twist');
 %% GENERAMOS EL MENSAJE
 msg = rosmessage(pub);
 % Velocidad lineal (solo se usa X)
-msg.Linear.X = -0.1;
+msg.Linear.X = 0.9;
 msg.Linear.Y = 0;
 msg.Linear.Z = 0;
 % Velocidad angular (solo se usa Z)
@@ -21,7 +21,7 @@ msg.Angular.Z = 0;
 %% Periodicidad del bucle de 10Hz
 rate = robotics.Rate(10);
 pause(1); % Pausamos para asegurarnos de que llega un mensaje
-while(strcmp(odom.LatestMessage.ChildFrameId,'robot0')~= 1)
+while(strcmp(odom.LatestMessage.ChildFrameId,'robot0') ~= 1)
     odom.LatestMessage
 end
 %% INICIALIZAMOS LA PRIMERA POSICION
@@ -31,29 +31,33 @@ posicionInicial = odom.LatestMessage.Pose.Pose.Position;
 listaDistancias = [];
 listaDiferencias = [];
 
-while(1)
+send(pub, msg);
+
+for i = 1:60
     posicionActual = odom.LatestMessage.Pose.Pose.Position;
     distanciaRecorrida = sqrt((posicionInicial.X - posicionActual.X)^2 + (posicionInicial.Y - posicionActual.Y)^2);
     listaDistancias = [listaDistancias, distanciaRecorrida];
-    send(pub, msg);
-    if(distanciaRecorrida > 3)
-        msg.Linear.X = 0;
-        send(pub, msg);
-        break;
-    end
-    waitfor(rate);
     disp(listaDistancias);
+    
+    waitfor(rate);
 end
+
+msg.Linear.X = 0;
+send(pub, msg);
 
 %% CALCULO DE DIFERENCIAS
 for index = 2 : length(listaDistancias)
     diferencia = listaDistancias(index) - listaDistancias(index - 1);
-    listaDiferencias = [listaDiferencias, diferencia];
+    if(diferencia ~= 0)
+        listaDiferencias = [listaDiferencias, diferencia];
+    end
 end
 
 disp(listaDiferencias);
 
+disp(min(listaDiferencias));
 disp(max(listaDiferencias));
+disp(mean(listaDiferencias));
 
 %% DESCONEXION DE ROS
 rosshutdown;
