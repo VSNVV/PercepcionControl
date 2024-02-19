ce11e164caa19ea1bd2952008f7ed173c3d8c343
viti <victor.sanavia@edu.uah.es>
2024-02-19 17:42:34 +0100

    Creación de SensorLaserC.m
    Este script coge mil medidas para luego dibujar una gráfica con todas ellas, responde al apartado 3 de la sección de cuestiones de sensores láser

---
+++ Práctica 1/SensoresLaser/SensorLaserC.m
@@ -0,0 +1,28 @@
+%% CONEXION A ROS
+setenv('ROS_MASTER_URI','http://192.168.1.166:11311') % IP de la MV
+setenv('ROS_IP','192.168.1.132') % IP de nuestro ordenador
+
+rosinit;
+
+%% SUBSCRIPCION AL LASER
+laser = rossubscriber('/robot0/laser_1');
+
+%% ALGORITMO
+
+listaDistancias = [];
+listaIteraciones = [];
+for i = 0:1000
+    mensajeLaser = receive(laser, 1);
+    distanciasLaser = mensajeLaser.Ranges;
+    listaDistancias = [listaDistancias, distanciasLaser];
+    listaIteraciones = [listaIteraciones, i];
+
+    disp(i);
+end
+
+plot(listaIteraciones, listaDistancias);
+xlabel('Iteraciones');
+ylabel('Distancias');
+
+%% DESCONEXION DE ROS
+rosshutdown;
