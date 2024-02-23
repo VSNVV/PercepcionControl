48e9a570109b33874660c61d1896604076af0435
viti <victor.sanavia@edu.uah.es>
2024-02-23 11:01:27 +0100

    Creación de SensorUltrasonidoCReal.m
    Script del apartado C para el robot real

---
+++ Práctica 1/SensoresUltrasonido/SensorUltrasonidoCReal.m
@@ -0,0 +1,31 @@
+%% CONEXION A ROS
+setenv('ROS_MASTER_URI','http://192.168.1.166:11311') % IP de la MV
+setenv('ROS_IP','192.168.1.132') % IP de nuestro ordenador
+
+%% INICIALIZACION DE ROS
+rosinit;
+
+%% SUBSCRIBERS
+sonar3 = rossubscriber('/sonar_2');
+
+%% ALGORITMO
+listaDistancias = [];
+listaIteraciones = [];
+
+for i = 0:1000
+
+    mensajeSonar = receive(sonar3, 1);
+    distanciaSonar = mensajeSonar.Range_;
+
+    listaDistancias = [listaDistancias, distanciaSonar];
+    listaIteraciones = [listaIteraciones, i];
+
+    disp(i);
+end
+
+plot(listaIteraciones, listaDistancias);
+xlabel('Tiempo');
+ylabel('Distancia');
+
+%% DESCONEXION DE ROS
+rosshutdown;
